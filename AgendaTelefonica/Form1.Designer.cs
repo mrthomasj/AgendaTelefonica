7b9ffc4a1cfa1d622eb0a7033f1cbd1753cc9f71
mrthomasj <tr.jehlen@outlook.com>
2019-09-18 05:37:30 -0300

    v1.0

---
+++ AgendaTelefonica/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace AgendaTelefonica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvMain = new System.Windows.Forms.DataGridView();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblTel1 = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblTel2 = new System.Windows.Forms.Label();
+            this.lblNote = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtTel1 = new System.Windows.Forms.TextBox();
+            this.txtTel2 = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtNote = new System.Windows.Forms.TextBox();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvMain
+            // 
+            this.dgvMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMain.Location = new System.Drawing.Point(12, 284);
+            this.dgvMain.Name = "dgvMain";
+            this.dgvMain.Size = new System.Drawing.Size(755, 249);
+            this.dgvMain.TabIndex = 0;
+            this.dgvMain.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvMain_CellClick);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(31, 56);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(57, 19);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Nome:";
+            // 
+            // lblTel1
+            // 
+            this.lblTel1.AutoSize = true;
+            this.lblTel1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTel1.Location = new System.Drawing.Point(31, 85);
+            this.lblTel1.Name = "lblTel1";
+            this.lblTel1.Size = new System.Drawing.Size(90, 19);
+            this.lblTel1.TabIndex = 2;
+            this.lblTel1.Text = "Telefone 1:";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(31, 145);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(60, 19);
+            this.lblEmail.TabIndex = 4;
+            this.lblEmail.Text = "E-mail:";
+            // 
+            // lblTel2
+            // 
+            this.lblTel2.AutoSize = true;
+            this.lblTel2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTel2.Location = new System.Drawing.Point(31, 115);
+            this.lblTel2.Name = "lblTel2";
+            this.lblTel2.Size = new System.Drawing.Size(90, 19);
+            this.lblTel2.TabIndex = 3;
+            this.lblTel2.Text = "Telefone 2:";
+            // 
+            // lblNote
+            // 
+            this.lblNote.AutoSize = true;
+            this.lblNote.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNote.Location = new System.Drawing.Point(31, 173);
+            this.lblNote.Name = "lblNote";
+            this.lblNote.Size = new System.Drawing.Size(97, 19);
+            this.lblNote.TabIndex = 5;
+            this.lblNote.Text = "Observação:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(136, 55);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(240, 20);
+            this.txtName.TabIndex = 6;
+            // 
+            // txtTel1
+            // 
+            this.txtTel1.Location = new System.Drawing.Point(136, 84);
+            this.txtTel1.Name = "txtTel1";
+            this.txtTel1.Size = new System.Drawing.Size(240, 20);
+            this.txtTel1.TabIndex = 7;
+            // 
+            // txtTel2
+            // 
+            this.txtTel2.Location = new System.Drawing.Point(136, 114);
+            this.txtTel2.Name = "txtTel2";
+            this.txtTel2.Size = new System.Drawing.Size(240, 20);
+            this.txtTel2.TabIndex = 8;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(136, 143);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(240, 20);
+            this.txtEmail.TabIndex = 9;
+            // 
+            // txtNote
+            // 
+            this.txtNote.Location = new System.Drawing.Point(136, 172);
+            this.txtNote.Name = "txtNote";
+            this.txtNote.Size = new System.Drawing.Size(240, 20);
+            this.txtNote.TabIndex = 10;
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(438, 52);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(112, 52);
+            this.btnNew.TabIndex = 11;
+            this.btnNew.Text = "Novo Contato";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.BtnNew_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(438, 140);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(112, 52);
+            this.btnUpdate.TabIndex = 12;
+            this.btnUpdate.Text = "Atualizar Contato";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.BtnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(593, 52);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(112, 52);
+            this.btnDelete.TabIndex = 13;
+            this.btnDelete.Text = "Deletar Contato";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(593, 140);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(112, 52);
+            this.btnSave.TabIndex = 14;
+            this.btnSave.Text = "Salvar Contato";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 545);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.txtNote);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtTel2);
+            this.Controls.Add(this.txtTel1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblNote);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblTel2);
+            this.Controls.Add(this.lblTel1);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.dgvMain);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvMain;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblTel1;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblTel2;
+        private System.Windows.Forms.Label lblNote;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtTel1;
+        private System.Windows.Forms.TextBox txtTel2;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtNote;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ AgendaTelefonica/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.Core.Objects;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace AgendaTelefonica
+{
+    public partial class Form1 : Form
+    {
+        Validations val = new Validations();
+        AgendaTelefonica.Models.ContactsEntity contextContacts;
+        List<AgendaTelefonica.Models.tb_contatos> contacts;
+        int curId = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ExibirContatos();
+        }
+
+
+        public void ExibirContatos()
+        {
+            contextContacts = new Models.ContactsEntity();
+            contacts = new List<Models.tb_contatos>();
+            //ObjectQuery<Models.tb_contatos> contactsQry = contextContacts.tb_contatos.;
+            contacts = contextContacts.tb_contatos.ToList();
+            dgvMain.DataSource = contextContacts.tb_contatos.ToList();
+
+            PreencheCampos();
+
+
+        }
+
+
+        public void PreencheCampos()
+        {
+            Models.tb_contatos curContact = contacts[curId];
+            txtName.Text = curContact.nmContato;
+            txtTel1.Text = curContact.nr_telPrinc;
+            txtTel2.Text = curContact.nr_telSec;
+            txtEmail.Text = curContact.email;
+            txtNote.Text = curContact.notes;
+        }
+
+        private void DgvMain_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            curId = dgvMain.CurrentRow.Index;
+            PreencheCampos();
+        }
+
+        private void BtnNew_Click(object sender, EventArgs e)
+        {
+            
+            val.clearAll(this);
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            Models.tb_contatos newCnt = new Models.tb_contatos();
+            newCnt.nmContato = txtName.Text;
+            newCnt.nr_telPrinc = txtTel1.Text;
+            newCnt.nr_telSec = txtTel2.Text;
+            newCnt.email = txtEmail.Text;
+            newCnt.notes = txtNote.Text;
+
+            contacts.Add(newCnt);
+            contextContacts.tb_contatos.Add(newCnt);
+            contextContacts.SaveChanges();
+            MessageBox.Show("Contato salvo com sucesso!");
+            ExibirContatos();
+            PreencheCampos();
+        }
+
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            Models.tb_contatos curCnt = contacts[curId];
+            curCnt.nmContato = txtName.Text;
+            curCnt.nr_telPrinc = txtTel1.Text;
+            curCnt.nr_telSec = txtTel2.Text;
+            curCnt.email = txtEmail.Text;
+            curCnt.notes = txtNote.Text;
+
+            contextContacts.SaveChanges();
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            if (!val.checkEmpty(this))
+                return;
+            Models.tb_contatos curCnt = contacts[curId];
+            curCnt.nmContato = txtName.Text;
+            curCnt.nr_telPrinc = txtTel1.Text;
+            curCnt.nr_telSec = txtTel2.Text;
+            curCnt.email = txtEmail.Text;
+            curCnt.notes = txtNote.Text;
+            contextContacts.tb_contatos.Remove(curCnt);
+            contextContacts.SaveChanges();
+            curId = 0;
+            ExibirContatos();
+            PreencheCampos();
+        }
+    }
+}
